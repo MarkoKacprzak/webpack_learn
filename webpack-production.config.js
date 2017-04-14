31b3aba998efbfad18d38cc0a2bdfbb3caead7c6
marek.kacprzak <marek.kacprzak@windowslive.com>
2017-04-14 20:00:22 +0200

    chapter3: reorganize webpack project + add dynamic js output to config

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,12 +1,14 @@
-var webPackStrip = require('strip-loader');
+//var webPackStrip = require('strip-loader');
 
 var devConfig = require('./webpack.config.js');
 
 var stripLoader = {
     test: [/\.js$/, /\.es6$/],
     exclude: /node_modules/,
-	loader: webPackStrip.loader('console.log')
+	//loader: webPackStrip.loader('console.log')
+    loader: "strip-loader?strip[]=debug,strip[]=console.log"
 }
 devConfig.module.loaders.push(stripLoader);
+devConfig.watch=false;
 
 module.exports = devConfig;
