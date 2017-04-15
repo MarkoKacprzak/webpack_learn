7f4cb3ddac2b4a15552ac79e35c94e86e7b828c6
marek.kacprzak <marek.kacprzak@windowslive.com>
2017-04-15 17:11:46 +0200

    webpack css-loader, style-loader, sass-loader, less-loader

---
+++ webpack-css/js/app.js
@@ -0,0 +1,9 @@
+console.log('App loaded');
+
+require('../css/bootstrap.css');
+//require('../css/app.css'); //css-loader  style-loader
+require('../css/app.scss'); //sass-loader
+//require('../css/app.less'); //less-loader
+
+
+
---
+++ webpack-css/webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+module.exports = {
+	context: path.resolve('js'),
+	entry: ["./app"],
+	output: {
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
+		filename: "bundle.js"
+	},
+
+	devServer: {
+		contentBase: 'public'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader"
+			},
+			/*
+			{
+				test: /\.less$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader!less-loader"
+			}
+				*/
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader!sass-loader"
+			}
+		]
+	},
+
+	resolve: {
+		extensions: ['.js', '.es6']
+	}
+}
